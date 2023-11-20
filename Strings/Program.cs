568edb9f5a8ec7640bec9806db3d4e334fe939d8
durgaprasad226 <durgaprasadmalthumkar@gmail.com>
2023-11-20 16:53:38 +0530

    Accept Commit

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -18,6 +18,16 @@ namespace Strings
             Console.WriteLine(msg2.Length);
             Console.WriteLine(msg1.ToUpper());                          //Converts all the characters into UPPERCASE....
             Console.WriteLine(msg2.ToLower());                          //Converts all the characters into lowercase....
+            Console.WriteLine(msg2.Trim());                             //Removes excess white spaces....
+            Console.WriteLine(msg1.Equals(msg2));                       //Checks whether two strings are same or not....
+            Console.WriteLine("==========================String Clone==========================");
+            string text1 = "Hello";
+            string text2=(string)text1.Clone();                         //Cloning string of one variable into another variable....
+            Console.WriteLine($"My text1 is {text1} and my text2 is {text2}");
+            Console.WriteLine("==========================Copy==========================");
+            string text3 = "Byeeee";
+            string text4=string.Copy(text3);                            //Copying string of one variable into another variable....
+            Console.WriteLine($"My text3 is {text3} and text4 is {text4}");
             Console.WriteLine("==============================String Concatenation==============================");
             string msg3=msg1 + msg2;                                    //Concatenation Method--1
             Console.WriteLine(msg3);
@@ -27,6 +37,38 @@ namespace Strings
             string firstName = "Durga Prasad";
             string lastName = "Malthumkar";
             Console.WriteLine($"My fullName is {firstName} {lastName}");
+            Console.WriteLine("==============================CompareOrdinal()==============================");
+            string s1 = "zebra";   
+            string s2 = "jello";                                        //compares two specified String objects by evaluating the 
+            string s3 = "hello";                                        //numeric values of corresponding Char objects in each string
+            string s4 = "apple";
+            Console.WriteLine(string.CompareOrdinal(s1,s2));
+            Console.WriteLine(string.CompareOrdinal(s1,s3));
+            Console.WriteLine(string.CompareOrdinal(s1,s4));
+            Console.WriteLine("================================EndsWith================================");
+            string n1 = "Prasad";
+            string n2 = "sad";
+            string n3 = "hii";
+            Console.WriteLine(n1.EndsWith(n2));                         //It is used to check that the end of this string instance 
+            Console.WriteLine(n1.EndsWith(n3));                         //matches the specified string.
+            Console.WriteLine("=================================GetHashCode================================");
+            string ghc = "Hello";
+            string ghc1 = "Who is this?";
+            string ghc2 = "This is Durga Prasad";
+            Console.WriteLine(ghc.GetHashCode());                       //GetHashCode() method is used to get hash code of this string. 
+            Console.WriteLine(ghc1.GetHashCode());                      //It returns an integer value.
+            Console.WriteLine(ghc2.GetHashCode());
+            Console.WriteLine("==============================IsNullOrEmpty===============================");
+            string ine = "Hi";
+            string ine1 = "";
+            string ine2 = " ";
+            Console.WriteLine(string.IsNullOrEmpty(ine));               //IsNullOrEmpty() checks whether the string is null or an Empty string. 
+            Console.WriteLine(string.IsNullOrEmpty(ine1));              //It returns a boolean value either true or false.
+            Console.WriteLine(string.IsNullOrWhiteSpace(ine2));         //Checks for the white spaces
+            Console.WriteLine("================================Insert==================================");
+            string i1 = "Hello DP";                                     //The C# Insert() method is used to insert the specified string 
+            string i2 = i1.Insert(5, "_");                              //at specified index number
+            Console.WriteLine($"Before insert {i1} after insert {i2}");
             Console.WriteLine("==============================Accessing Strings================================");
             string myName = "Durga Prasad";
             Console.WriteLine(myName[8]);                               //Access the string using index position....
@@ -34,8 +76,8 @@ namespace Strings
             Console.WriteLine("====================================Substring==================================");
             string name = "Durga Prasad";
             int charpos=name.IndexOf("P");
-            //int charpos=name.IndexOf("r");
-            //int charpos=name.LastIndexOf("r");
+            //int charpos=name.IndexOf("r");                            //Shows the index of first "r"....
+            //int charpos=name.LastIndexOf("r");                        //Shows the index of last occured "r"....
             string Name =name.Substring(charpos);
             Console.WriteLine(Name);
             Console.WriteLine(name.Contains("D"));                      //Checks whether the given character is present or not....
